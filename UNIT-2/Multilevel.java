e2bae38f7296619ac69daab5698a87cce58152f9
MD.AASIF <mdaasifnic406@gmail.com>
2026-08-06 15:33:09 +0530

    Upload Unit 2 Java Programs

---
+++ UNIT-2/Multilevel.java
@@ -0,0 +1,37 @@
+class Animal
+{
+    String name = "Animal";
+
+    void eat()
+    {
+        System.out.println("The dog can eat.");
+    }
+}
+
+class Dog extends Animal
+{
+    void bark()
+    {
+        System.out.println("The dog can bark: Bhaou Bhaou");
+    }
+}
+
+class Puppy extends Dog
+{
+    void weep()
+    {
+        System.out.println("The puppy is weeping.");
+    }
+}
+
+public class Multilevel
+{
+    public static void main(String args[])
+    {
+        Puppy p = new Puppy();
+
+        p.eat();
+        p.bark();
+        p.weep();
+    }
+}
