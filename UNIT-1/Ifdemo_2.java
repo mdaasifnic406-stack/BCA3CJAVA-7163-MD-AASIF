a0907d0b7f41bd9441bc23f0217445be475fc55a
MD.AASIF <mdaasifnic406@gmail.com>
2026-08-14 16:54:49 +0530

    Add files via upload

---
+++ UNIT-1/Ifdemo_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ifdemo_2 {
+	public static void main(String[]args){
+		Scanner sc= new Scanner(System.in);
+		int rank=1;
+		
+		//condition: checks if rank is 1//
+		if (rank==1){
+			System.out.println("result: top in the table!");
+		}
+		System.out.println("execution complete");
+	}
+}
