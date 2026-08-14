ccc4445541271232c4fdf64b37968005f879836c
MD.AASIF <mdaasifnic406@gmail.com>
2026-08-14 17:07:56 +0530

    Add files via upload

---
+++ UNIT-1/my_introduction.java
@@ -0,0 +1,8 @@
+class my_introduction{
+public static void main(String[] args){
+System.out.println("Myself Md Aasif");
+System.out.println("I am from India");
+System.out.println("Right now I am pursuing BCA from Marwadi University, Rajkot");
+System.out.println("Thank you");
+}
+}
