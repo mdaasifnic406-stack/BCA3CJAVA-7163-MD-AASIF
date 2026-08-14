86e791aa07c4fcf3f29cc38de84a418d2c197343
MD.AASIF <mdaasifnic406@gmail.com>
2026-08-14 17:03:52 +0530

    Add files via upload

---
+++ UNIT-1/prog11.java
@@ -0,0 +1,13 @@
+public class prog11{
+
+public static void main(String[] args){
+
+  int[] marks = {1,2,3,4,5};
+  
+  
+  
+  for(int i=0;i<marks.length;i++){
+     System.out.print(marks[i]);
+  }
+}
+}
