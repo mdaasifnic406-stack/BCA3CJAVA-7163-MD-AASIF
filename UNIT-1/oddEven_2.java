8a8e6cd533bf400affbf72df825d8581b4ced3e0
MD.AASIF <mdaasifnic406@gmail.com>
2026-08-14 17:01:25 +0530

    Add files via upload

---
+++ UNIT-1/oddEven_2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class oddEven_2 {
+	public static void main(String args[]){
+		Scanner sc= new Scanner(System.in);
+		int num=3;
+		
+	// check whether the given number is even or odd
+	if (num%2==0){
+		System.out.println("the given number is even!");
+	}else{
+		System.out.println("the given number is odd!");
+		}
+		System.out.println("execution complete");
+		
+	} 
+} 
+		
+	
